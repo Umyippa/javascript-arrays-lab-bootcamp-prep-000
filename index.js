05fcf025dcaee095557d7ad6b275f009c0843dde
Umyippa <sywhitehouse@gmail.com>
2018-06-16 20:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(kittens) {
-  (kittens).Appendkitten("Ralph");
+  (kittens).Appendkitten();
   return kittens;
 }
